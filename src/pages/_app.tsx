efb72257fde1d7233190ab919ed99c27cec9f8e4
mustafa.zakaria <mustafa.zakaria@mygom.tech>
2024-04-13 16:01:09 +0300

    initialize project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { legacyTheme } from '@/components/theme';
+import { theme } from '@/theme';
+import Fonts from '@/theme/components/Fonts';
+import { AppPropsWithLayout } from '@/types/layout';
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? (page => page);
+
+  const pageWithLayout = getLayout(<Component {...pageProps} />);
+
+  return (
+    <>
+      <Head>
+        <title>Mustafa Portfolio</title>
+        {/* Add any other meta tags or links here */}
+      </Head>
+      <Fonts />
+      <ChakraProvider theme={theme}>
+        <ThemeProvider theme={legacyTheme}>{pageWithLayout}</ThemeProvider>
+      </ChakraProvider>
+    </>
+  );
 }
