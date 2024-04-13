1defdc405fb3dae32b670b6d3354944daa942d6a
mustafa.zakaria <mustafa.zakaria@mygom.tech>
2024-04-13 16:53:33 +0300

    fix: header sticky

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,22 +1,54 @@
+// Header component
+import { scrollToSection } from '@/utils/scroll';
 import { Box, Flex, Heading, Link, Spacer } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useState, useEffect } from 'react';
 
 const NAV_LINKS = ['Services', 'Skills', 'Projects', 'Hobbies'];
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Box px='1rem' as='header' borderColor='gray.200' py='4' h='5rem' backgroundColor='#191d24'>
+    <Box
+      px='1rem'
+      as='header'
+      borderColor='gray.200'
+      h='5rem'
+      backgroundColor='primary0'
+      position={isSticky ? 'sticky' : 'relative'}
+      top={0}
+      left={0}
+      right={0}
+      zIndex={999}
+    >
       <Flex maxW='container.xl' mx='auto' justifyContent='space-between' alignItems='center' h='100%'>
-        <NextLink href='/' passHref>
-          <Link as='span'>
-            <Heading size='md' color='primary1'>
-              My portfolio
-            </Heading>
-          </Link>
-        </NextLink>
+        <Link as='span' onClick={() => scrollToSection('home')}>
+          <Heading size='md' color='primary1'>
+            My portfolio
+          </Heading>
+        </Link>
         <Flex display={{ base: 'none', md: 'flex' }}>
           {NAV_LINKS.map(link => (
-            <Link key={link} color='primary1' as='span' mr='4'>
+            <Link key={link} color='primary1' as='span' mr='1.5rem' onClick={() => scrollToSection(link.toLowerCase())}>
               {link}
             </Link>
           ))}
